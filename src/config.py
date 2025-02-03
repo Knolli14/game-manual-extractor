18ee790987ee81d5247141a26145d981f0f4d619
Santa Knolli <olli.coding@proton.me>
2025-02-03 15:01:33 +0100

    added rootDir, dataDir and URL to searchPage

---
+++ src/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# First page of empty search
+SEARCH_URL = "https://en.1jour-1jeu.com/rules/search?"
+
+# Paths
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+MANUALS_DIR = BASE_DIR / "data"
